d375ed776934e046b267651b822fcbb3ef8e7969
Görkem Özdemir <55491306+Grkmzdmr@users.noreply.github.com>
2023-04-29 09:31:56 +0300

    Update strings.dart

--- lib/core/constants/strings.dart
+++ lib/core/constants/strings.dart
@@ -1,6 +1,6 @@
 class Strings {
-  //static const String baseUrl = "https://notsosecret.snowsparrow.com/";
-  static const String baseUrl = "https://survey.snowsparrow.com/";
+ 
+  static const String baseUrl = "https://write-your-api-adress/";
   static const String loginUrl = baseUrl + "basic/login";
   static const String getReviewsUrl = baseUrl + "basic/get-surveys";
   static const String getQuestions = baseUrl + "basic/get-questions/:";
